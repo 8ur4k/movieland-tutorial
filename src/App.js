8f4cdf48f2696aaad0295ecfec1b66c4524da636
Burak Ilgenci <burakonduty@MacBook-Pro-2.local>
2023-02-27 15:42:53 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  return <h1>APP</h1>;
+};
 
 export default App;
